db82997e38051d7fdb15ec09f4a2be0cb19b718c
Radoslav Boydev <radoslav.boydev@gmail.com>
2025-04-25 02:17:34 +0300

    Added Laravel Pint

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,7 @@
 
 namespace Tests;
 
-//use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+// use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
